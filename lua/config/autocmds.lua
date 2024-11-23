f938f4c3f77077f57e45f881ef1e294a7ed845e7
Suthiphong .T <suthiphong.t@arise.tech>
2024-11-23 22:20:10 +0700

    initial commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,8 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+--
+vim.cmd("hi Normal guibg=#000000") -- UNCOMMENT THIS FOR BACKGROUND COLOR
+-- setting highlight Visual
+
+-- vim.cmd("hi Visual guibg=#000000 guifg=#ffffff")
